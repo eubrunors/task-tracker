6ab0855052c29eb659ee142f1082a7d46e518484
eubrunors <bruno.2013.br11@gmail.com>
2025-03-30 04:25:58 -0300

    :sparkles: feat(Task): Add filtering for 'list' command and implement 'mark-in-progress' & 'mark-done'

--- Task.php
+++ Task.php
@@ -1,57 +1,54 @@
 <?php
 //some header
 //namespace
-class Task{
+class Task {
 
-A   private string $action;
+    private string $action;
     private int $taskID;
     private string $message;
     private const string FILE_PATH = 'task.json';
 
-    public function __construct(string $action, int $taskID = 0, string $message = ''){
+    public function __construct(string $action, int $taskID = 0, string $message = '') {
         $this->action = $action;
         $this->taskID = $taskID;
         $this->message = $message;
     }
 
-    private function loadTasks(): array
-    {
+    private function loadTasks(): array {
         return file_exists(self::FILE_PATH) ? json_decode(file_get_contents(self::FILE_PATH), true) : [];
     }
 
-    private function saveTasks(array $tasks): void
-    {
+    private function saveTasks(array $tasks): void {
         file_put_contents(self::FILE_PATH, json_encode($tasks, JSON_PRETTY_PRINT));
     }
 
-    public function displayTask(): array{
-        return[
+    public function displayTask(): array {
+        return [
             "action" => $this->action,
             "taskID" => $this->taskID,
             "message" => $this->message
         ];
     }
 
-    public function getAction():void {
-        if( $this -> action === "add"){
-            $this->addTask();
-        }
-        elseif ( $this -> action === "update"){
-            $this->updateTask();
-        }
-        elseif ( $this->action === "delete"){
-            $this->deleteTask();
-        }elseif ( $this->action == "list"){
-            $this->listAllTasks();
-        }
-        else{
+    public function getAction(): void {
+        if ($this->action === "add") {  $this->addTask();
+        } elseif ($this->action === "update") { $this->updateTask();
+        } elseif ($this->action === "delete") { $this->deleteTask();
+        } elseif ($this->action === "mark-done") { $this->markDoneTask();
+        } elseif ($this->action === "mark-todo") { $this->markTodoTask();
+        } elseif ($this->action === "mark-in-progress") { $this->markInProgressTask();
+        } elseif ($this->action === "list" && $this->message === ' ') { $this->listAllTasks();
+        } elseif ($this->action === "list" && $this->message === 'done') { $this->listTasksByStatus();
+        } elseif ($this->action === "list" && $this->message === 'todo') { $this->listTasksByStatus();
+        } elseif ($this->action === "list" && $this->message === 'in-progress') { $this->listTasksByStatus();
+        } else {
             echo "error: action not known";
         }
     }
 
-    public function addTask():void{
+    public function addTask(): void {
         $tasks = $this->loadTasks();
-        $newID = count($tasks) > 0 ? (end($tasks)['id']+1) : 1;
+        $newID = count($tasks) > 0 ? (end($tasks)['id'] + 1) : 1;
 
         $newTask = [
             "id" => $newID,
@@ -67,38 +64,39 @@ A   private string $action;
 
         echo "Output: Task added successfully (ID: $newID)\n";
     }
-    public function updateTask(): void{
+
+    public function updateTask(): void {
         $tasks = $this->loadTasks();
 
         $taskFound = false;
-        foreach($tasks as &$task){
-            if($task['id'] === $this -> taskID){
-                $task['description'] = $this -> message;
+        foreach ($tasks as &$task) {
+            if ($task['id'] === $this->taskID) {
+                $task['description'] = $this->message;
                 $task['updatedAt'] = date("Y-m-d H:i:s");
                 $this->saveTasks($tasks);
                 $taskFound = true;
                 break;
             }
         }
-        if($taskFound){
+        if ($taskFound) {
             echo "Output: Task updated successfully (ID: $this->taskID)\n";
         } else {
             echo "Task with ID: $this->taskID not found\n";
         }
     }
 
-    public function deleteTask(): void{
+    public function deleteTask(): void {
         $tasks = $this->loadTasks();
         $taskFound = false;
 
-        foreach($tasks as $key => $task){
-            if($task['id'] === $this -> taskID){
+        foreach ($tasks as $key => $task) {
+            if ($task['id'] === $this->taskID) {
                 unset($tasks[$key]);
                 $taskFound = true;
                 break;
             }
         }
-        if($taskFound){
+        if ($taskFound) {
             $this->saveTasks(array_values($tasks));
             echo "Output: Task deleted successfully (ID: $this->taskID)\n";
 
@@ -107,12 +105,12 @@ A   private string $action;
         }
     }
 
-    public function listAllTasks(): void{
+    public function listAllTasks(): void {
         $tasks = $this->loadTasks();
 
-        if(!empty($tasks)){
+        if (!empty($tasks)) {
             echo "Listing all tasks:\n";
-            foreach($tasks as $task){
+            foreach ($tasks as $task) {
                 echo "id: $task[id]\n";
                 echo "description: $task[description]\n";
                 echo "status: $task[status]\n";
@@ -121,8 +119,86 @@ A   private string $action;
                 echo "----------------------------------\n";
             }
 
-        }else{
+        } else {
             echo "No tasks found\n";
+        }
+    }
+
+    public function listTasksByStatus(): void {
+        $tasks = $this->loadTasks();
+
+        if (!empty($tasks)) {
+            echo "Listing $this->message tasks:\n";
+            foreach ($tasks as $task) {
+                if ($this->message === $task['status']) {
+                    echo "id: $task[id]\n";
+                    echo "description: $task[description]\n";
+                    echo "status: $task[status]\n";
+                    echo "createdAt: $task[createdAt]\n";
+                    echo "updatedAt: $task[updatedAt]\n";
+                    echo "----------------------------------\n";
+
+                }
+            }
+        } else {
+            echo "No tasks found\n";
+        }
+    }
+
+    public function markDoneTask(): void {
+        $tasks = $this->loadTasks();
+        $taskFound = false;
+        foreach ($tasks as &$task) {
+            if ($task['id'] === $this->taskID) {
+                $task['status'] = 'done';
+                $task['updatedAt'] = date("Y-m-d H:i:s");
+                $this->saveTasks($tasks);
+                $taskFound = true;
+                break;
+            }
+        }
+        if ($taskFound) {
+            echo "Output: Task done successfully (ID: $this->taskID)\n";
+        } else {
+            echo "Task with ID: $this->taskID not found\n";
+        }
+    }
+
+    public function markInProgressTask(): void {
+        $tasks = $this->loadTasks();
+        $taskFound = false;
+        foreach ($tasks as &$task) {
+            if ($task['id'] === $this->taskID) {
+                $task['status'] = 'in-progress';
+                $task['updatedAt'] = date("Y-m-d H:i:s");
+                $this->saveTasks($tasks);
+                $taskFound = true;
+                break;
+            }
+        }
+        if ($taskFound) {
+            echo "Output: Task in-progress successfully (ID: $this->taskID)\n";
+        } else {
+            echo "Task with ID: $this->taskID not found\n";
+        }
+    }
+
+    public function markTodoTask(): void {
+        $tasks = $this->loadTasks();
+        $taskFound = false;
+        foreach ($tasks as &$task) {
+            if ($task['id'] === $this->taskID) {
+                $task['status'] = 'todo';
+                $task['updatedAt'] = date("Y-m-d H:i:s");
+                $this->saveTasks($tasks);
+                $taskFound = true;
+                break;
+            }
+        }
+        if ($taskFound) {
+            echo "Output: Task todo successfully (ID: $this->taskID)\n";
+        } else {
+            echo "Task with ID: $this->taskID not found\n";
         }
     }
 }
--- task-cli.php
+++ task-cli.php
@@ -1,30 +1,54 @@
 <?php
 
-    require_once 'Task.php';
+require_once 'Task.php';
 
-    // add or update or delete or list
-    $action = isset($argv[1]) ? (string)$argv[1] : ' ';
+// add or update or delete or list
+$action = isset($argv[1]) ? (string)$argv[1] : ' ';
 
-    if ($argv[1]==="add"){
-        $taskID = 0;
-        $message = isset($argv[2]) ? implode(' ',array_slice($argv,2)) : '';
+if ($argv[1] === "add") {
+    $taskID = 0;
+    $message = isset($argv[2]) ? implode(' ', array_slice($argv, 2)) : '';
 
-    } elseif ($argv[1] === "update"){
-        $taskID = isset($argv[2]) ?  (int)$argv[2] : 0;
-        $message = isset($argv[3]) ? implode(' ',array_slice($argv,3)) : '';
+} elseif ($argv[1] === "update") {
+    $taskID = isset($argv[2]) ? (int)$argv[2] : 0;
+    $message = isset($argv[3]) ? implode(' ', array_slice($argv, 3)) : '';
 
-    } elseif ($argv[1] === "delete") {
-        $taskID = isset($argv[2]) ? (int)$argv[2] : 0;
-        $message = ' ';
+} elseif ($argv[1] === "delete") {
+    $taskID = isset($argv[2]) ? (int)$argv[2] : 0;
+    $message = ' ';
 
-    }elseif ($argv[1] === "list"){
-        $taskID = 0;
-        $message = ' ';
+} elseif ($argv[1] === "list" && (!isset($argv[2]))) {
+    $taskID = 0;
+    $message = ' ';
 
-    }else{
-        echo "Invalid action.\n";
-        exit;
-    }
+} elseif ($argv[1] === "list" && $argv[2] === 'done') {
+    $taskID = 0;
+    $message = 'done';
 
-    $task = new Task($action, $taskID, $message);
-    $task->getAction();
+} elseif ($argv[1] === "list" && $argv[2] === 'in-progress') {
+    $taskID = 0;
+    $message = 'in-progress';
+
+} elseif ($argv[1] === "list" && $argv[2] === 'todo') {
+    $taskID = 0;
+    $message = 'todo';
+
+} elseif ($argv[1] === "mark-done") {
+    $taskID = (isset($argv[2])) ? (int)$argv[2] : 0;
+    $message = 'done';
+
+} elseif ($argv[1] === "mark-in-progress") {
+    $taskID = (isset($argv[2])) ? (int)$argv[2] : 0;
+    $message = 'in progress';
+
+} elseif ($argv[1] === "mark-todo") {
+    $taskID = (isset($argv[2])) ? (int)$argv[2] : 0;
+    $message = 'todo';
+
+} else {
+    echo "Invalid action.\n";
+    exit;
+}
+
+$task = new Task($action, $taskID, $message);
+$task->getAction();
