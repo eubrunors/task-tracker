095311f5ddd57aec1eb5129935ec7ad178d4b037
eubrunors <bruno.2013.br11@gmail.com>
2025-03-31 01:11:02 -0300

    :hammer: refactor(cli): Handle specific cases with missing arguments and return a log and remove redundancies

--- task-cli.php
+++ task-cli.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require_once 'Task.php';
@@ -8,14 +9,30 @@ $action = isset($argv[1]) ? (string)$argv[1] : ' ';
 if ($argv[1] === "add") {
     $taskID = 0;
     $message = isset($argv[2]) ? implode(' ', array_slice($argv, 2)) : '';
+    if(empty($message)) {
+        echo "Arguments Invalid: Missing task Description\n";
+        echo "Expected arguments: task-cli [command] (string)[new description]\n";
+        exit(1);
+
+    }
 
 } elseif ($argv[1] === "update") {
     $taskID = isset($argv[2]) ? (int)$argv[2] : 0;
     $message = isset($argv[3]) ? implode(' ', array_slice($argv, 3)) : '';
+    if ($taskID === 0 || $message === '' || !isset($message)) {
+        echo "Arguments Invalid: Missing task ID or Description\n";
+        echo "Expected arguments: task-cli [command] (int)[id] (string)[new description]\n";
+        exit(1);
+    }
 
 } elseif ($argv[1] === "delete") {
     $taskID = isset($argv[2]) ? (int)$argv[2] : 0;
     $message = ' ';
+    if ($taskID === 0) {
+        echo "Arguments Invalid: Missing task ID  \n";
+        echo "Expected arguments: task-cli [command] (int)[id] (string)[new description]\n";
+        exit(1);
+    }
 
 } elseif ($argv[1] === "list" && (!isset($argv[2]))) {
     $taskID = 0;
@@ -33,21 +50,21 @@ if ($argv[1] === "add") {
     $taskID = 0;
     $message = 'todo';
 
-} elseif ($argv[1] === "mark-done") {
-    $taskID = (isset($argv[2])) ? (int)$argv[2] : 0;
-    $message = 'done';
+} elseif ($argv[1] === "mark-done" ||
+    $argv[1] === "mark-in-progress" ||
+    $argv[1] === "mark-todo") {
 
-} elseif ($argv[1] === "mark-in-progress") {
     $taskID = (isset($argv[2])) ? (int)$argv[2] : 0;
-    $message = 'in progress';
-
-} elseif ($argv[1] === "mark-todo") {
-    $taskID = (isset($argv[2])) ? (int)$argv[2] : 0;
-    $message = 'todo';
+    $message = '';
+    if ($taskID === 0) {
+        echo "Arguments Invalid: Missing task ID  \n";
+        echo "Expected arguments: task-cli [command] (int)[id] (string)[new description]\n";
+        exit(1);
+    }
 
 } else {
     echo "Invalid action.\n";
-    exit;
+    exit(1);
 }
 
 $task = new Task($action, $taskID, $message);
