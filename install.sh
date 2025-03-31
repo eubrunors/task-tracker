a71a935888c8f7cf7de0e05e46e217b4bd54e17c
eubrunors <bruno.2013.br11@gmail.com>
2025-03-31 01:16:21 -0300

    :wrench: build: Add configuration file to use CLI

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! grep -q "alias task-cli=" ~/.bashrc; then
+    echo "Adding alias to ~/.bashrc"
+    echo "alias task-cli='$(pwd)/task-cli.php'" >> ~/.bashrc
+    echo "Alias added, please run: 'source ~/.bashrc' "
+    echo "And then use 'task-cli' to run app."
+
+    # Garante que o script task-cli.php tenha permissão de execução
+    chmod +x task-cli.php
+
+    echo "Permissions set, you can now use 'task-cli'."
+    echo "Please run: 'source ~/.bashrc' if you haven't already."
+else 
+    echo "Alias already exists in ~/.bashrc"
+fi
+
